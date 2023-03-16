c0e4da688fa32ada7857751a68b609ab0cbe35a7
Nirmal <nirmal.patel@canopusinfosystems.com>
2023-03-16 14:29:17 +0530

    Initial commit

---
+++ FrameGreeter/FrameGreeter/FrameGreeter.h
@@ -0,0 +1,18 @@
+//
+//  FrameGreeter.h
+//  FrameGreeter
+//
+//  Created by IOS on 16/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameGreeter.
+FOUNDATION_EXPORT double FrameGreeterVersionNumber;
+
+//! Project version string for FrameGreeter.
+FOUNDATION_EXPORT const unsigned char FrameGreeterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameGreeter/PublicHeader.h>
+
+
---
+++ FrameGreeter/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  FrameGreeter
+//
+//  Created by IOS on 16/03/23.
+//
+
+import Foundation
+
+public class Service {
+    public init() {}
+    public func greetAll() -> String {
+        return "Hello world!"
+    }
+}
